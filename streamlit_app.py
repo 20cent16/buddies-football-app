95d326554f59b1e82e78b3e3a98ffcf5ef759f49
20cent <112479170+20cent16@users.noreply.github.com>
2025-04-13 22:13:01 +0000

    revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,12 +103,6 @@ def main():
     if st.session_state.combo:
         df_filtered = df_filtered[df_filtered['combo'].str.contains(st.session_state.combo, case=False, na=False)]
 
-    # Ajouter une colonne de classement dynamique
-    df_filtered['Classement'] = df_filtered['total_matchs'].rank(ascending=False, method='min')
-
-    # Réorganiser les colonnes pour que la colonne de classement soit en premier
-    df_filtered = df_filtered[['Classement'] + [col for col in df_filtered.columns if col != 'Classement']]
-
     # Afficher le titre avec une taille réduite, un ballon devant et centré
     st.markdown("<h3 style='text-align: center;'>⚽ Résultats par combinaison de joueurs</h3>", unsafe_allow_html=True)
     st.dataframe(df_filtered, use_container_width=True, hide_index=True)
