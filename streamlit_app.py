d03b0cbac84b66e26c1bf5fc9712729ef7e43d2c
20cent <112479170+20cent16@users.noreply.github.com>
2025-04-15 20:09:11 +0000

    chgt dates

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,16 @@ def main():
     colnames = [desc[0] for desc in cur.description]
     df = pd.DataFrame(rows, columns=colnames)
 
-    query_sql = 'SELECT * FROM public.series ORDER BY start_date'
+    query_sql = 'SELECT * FROM public.series ORDER BY debut'
     cur = conn.cursor()
     cur.execute(query_sql)
     rows = cur.fetchall()
     colnames = [desc[0] for desc in cur.description]
     df_series = pd.DataFrame(rows, columns=colnames)
+
+    # 👉 Conversion des timestamps en format date lisible
+df_series['debut'] = pd.to_datetime(df_series['debut']).dt.strftime('%d/%m/%Y')
+df_series['fin'] = pd.to_datetime(df_series['fin']).dt.strftime('%d/%m/%Y')
 
     # Initialiser les états
     if 'nb_joueurs' not in st.session_state:
